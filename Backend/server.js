e656aea0581b95d9018d9d4cfc5a1d427c2bb563
project-3 <your-github-pruthvirajul2001@gmail.com@example.com>
2025-07-17 10:37:02 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -164,5 +164,5 @@ app.delete('/api/claims', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://13.61.21.141:${port}`);
+    console.log(`Server running at http://13.49.238.64:${port}`);
 });
